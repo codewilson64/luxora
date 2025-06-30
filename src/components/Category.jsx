7f22770d4c46940beff28bbbc2638b26db8e5bcd
codewilson64 <wilsongambit@gmail.com>
2025-06-30 15:27:11 +0700

    first commit

---
+++ src/components/Category.jsx
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+
+const Category = () => {
+  const [category, setCategory] = useState([])
+
+  useEffect(() => {
+    const fetchCategory = async () => {
+      try {
+        const response = await fetch('/category.json')
+        const data = await response.json()
+        console.log(data)
+        setCategory(data)
+      } 
+      catch (error) {
+        console.log('Fetch category failed', error)  
+      }
+    }
+
+    fetchCategory()
+  }, [])
+
+  return (
+    <div className='py-5'>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-1'>
+        {category.map((item) => (
+          <Link
+            to={`/${item.category}`}
+            key={item.id} 
+            className='relative w-full cursor-pointer'
+          >
+            <img 
+              src={item.image} 
+              alt="item image" 
+              className='w-full h-full object-cover'
+            />            
+            <p className='w-full text-center absolute bottom-4 left-1/2 -translate-x-1/2 text-lg text-black font-normal'>
+              {item.name}
+            </p>           
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Category
---
+++ src/components/CheckoutBox.jsx
@@ -0,0 +1,46 @@
+import { useSelector } from 'react-redux'
+import { toast } from 'react-hot-toast'
+
+const CheckoutBox = () => {
+  const subTotal = useSelector(state => state.cart.cartTotalAmount)
+   
+  return (
+    <div className='h-fit w-full md:w-[35%] bg-zinc-300/40 rounded-lg px-5 py-5 mt-5 md:mt-0'>
+          <div className='w-full text-center border-b border-gray-300 pb-5'>
+            <h1 className="text-xl text-center font-bold text-black uppercase">order summary</h1>
+          </div>
+
+          <div className='flex items-center justify-between font-normal text-black py-5 border-b border-gray-300'>
+            <div className='text-sm'>
+              <p>Subtotal</p>
+            </div>
+
+            <div className='text-lg'>
+              <p>${subTotal.toFixed(2)}</p>
+            </div>
+          </div>
+
+          <div className='text-lg text-black py-5 border-b border-gray-300'>
+            <div className='flex items-center justify-between font-bold mb-4'>
+              <div>
+                <p>Estimated Total:</p>
+              </div>
+
+              <div>
+                <p>${subTotal.toFixed(2)}</p>
+              </div>
+            </div>
+
+            <div 
+              onClick={() => toast.success("Checkout Success")}
+              className='bg-green-500 py-2 text-center rounded-lg'
+              >
+              <button className='uppercase text-sm text-white'>proceed to checkout</button>
+            </div>
+          </div>
+
+        </div>
+  )
+}
+
+export default CheckoutBox
